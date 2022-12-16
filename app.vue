34c4117af8b008411a9ac92eaa5dc1c25c65087a
appert <scoreur@gmail.com>
2022-12-16 11:53:21 +0100

    ajout data, name,card,option

--- app.vue
+++ app.vue
@@ -3,76 +3,79 @@
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" buttom below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-        <div class="option-buttons">
-          <button class="option option-left"  :class="options.gender === Gender.Boy && 'option-active'">Boy</button>
-          <button class="option"  :class="options.gender === Gender.Unisex && 'option-active'">Unisex</button>
-          <button 
-          class="option option-right"
-          :class="options.gender === 'Girl' && 'option-active'"
-          >
-          Girl</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose the name' popularity</h4>
-        <div class="option-buttons">
-          <button class="option option-left"  :class="options.popularity === Popularity.Trendy && 'option-active'">Trendy</button>
-          <button class="option option-right" :class="options.popularity === Popularity.Unique && 'option-active'">Unique</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>1) Choose name's length</h4>
-        <div class="option-buttons">
-          <button class="option option-left" :class="options.length === Length.Long && 'option-active'">Long</button>
-          <button class="option" :class="options.length === Length.All && 'option-active'">All</button>
-          <button class="option option-right" :class="options.length === Length.Short && 'option-active'">Short</button>
-        </div>
-      </div>
+      <Option
+        v-for="option in optionsArray"
+        :key="option.title"
+        :option="option"
+        :options="options"
+      />
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    </div>
+    <div class="cards-container">
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="() => removeName(index)"
+        :index="index"
+      />
     </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
-
-enum Gender {
-  Girl = 'Girl',
-  Boy = 'Boy',
-  Unisex = 'Unisex'
-}
-enum Popularity {
-  Trendy = 'Trendy',
-  Unique = 'Unique',
-  
-}
-enum Length {
-  Long = 'Long',
-  All = 'All',
-  Short = 'Short'
-}
+import { Gender, Popularity, Length, names } from "./data";
 
 interface OptionsState {
-  gender: Gender,
-  popularity: Popularity,
-  length: Length,
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
 }
-
-const obj: OptionsState = {
-  gender: Gender.Girl,
-  popularity: Popularity.Trendy,
-  length: Length.All,
-}
-
-// const options: OptionsState = reactive ou  const options = reactive<OptionsState>  === pareil !
 
 const options = reactive<OptionsState>({
-  gender: Gender.Girl,
-  popularity: Popularity.Trendy,
-  length: Length.All,
-})
+  gender: Gender.GIRL,
+  length: Length.SHORT,
+  popularity: Popularity.TRENDY,
+});
 
+const computeSelectedNames = () => {
+  const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true;
+      else return name.length === options.length;
+    });
+
+  selectedNames.value = filteredNames.map((name) => name.name);
+};
+
+const selectedNames = ref<string[]>([]);
+
+const removeName = (index: number) => {
+  const filteredNames = [...selectedNames.value];
+  filteredNames.splice(index, 1);
+  selectedNames.value = filteredNames;
+};
+
+const optionsArray = [
+  {
+    title: "1) Choose a gender",
+    category: "gender",
+    buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+  },
+  {
+    title: "2) Choose the name's popularity",
+    category: "popularity",
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+  },
+  {
+    title: "3) Choose name's length",
+    category: "length",
+    buttons: [Length.SHORT, Length.ALL, Length.LONG],
+  },
+];
 </script>
 
 
@@ -85,6 +88,7 @@ const options = reactive<OptionsState>({
   margin: 0 auto;
   text-align: center;
 }
+
 h1 {
   font-size: 3rem;
 }
@@ -97,31 +101,21 @@ h1 {
   margin-top: 4rem;
   position: relative;
 }
-.option-container {
-  margin-bottom: 2rem;
-}
 
-.option {
-  background: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-  cursor: pointer;
-  font-weight: 200;
-}
-.option-left{
-  border-radius: 1rem 0 0 1rem;
-}
-.option-right{
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
+.primary {
   background-color: rgb(249, 87, 89);
   color: white;
+  border-radius: 6.5rem;
+  border: none;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
 }
 
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
 </style>
